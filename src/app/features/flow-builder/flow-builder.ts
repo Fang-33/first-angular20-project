10b0d6f2cf478ed898ffd8bf32928cfd1769f24e
Fang-33 <33justworkplace@gmail.com>
2026-03-04 17:44:53 +0800

    feat: add two node and make it dragable

--- src/app/features/flow-builder/flow-builder.ts
+++ src/app/features/flow-builder/flow-builder.ts
@@ -1,9 +1,25 @@
+import { CdkDrag } from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-flow-builder',
-  imports: [],
+  imports: [CdkDrag],
   templateUrl: './flow-builder.html',
   styleUrl: './flow-builder.scss',
 })
-export class FlowBuilder {}
+export class FlowBuilder {
+  // ===== inject =====
+  // ===== method decorator =====
+  // ===== viewChild =====
+  // ===== signal input =====
+  // ===== signal output =====
+  // ===== prop & const =====
+  // ===== signal ======
+  // ===== signal computed =====
+  // ===== signal effect =====
+  // ===== lifecycle hooks =====
+  // ===== initialization =====
+  // ===== catch event =====
+  // ===== catch event functions =====
+  // ===== getters =====
+}
